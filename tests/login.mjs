28adbc44851faefbefcb3e1e47cbaf7b4d174354
Aditya Reddy <aditya@gmail.com>
2025-02-15 18:53:13 +0530

    test file updated

--- tests/login.mjs
+++ tests/login.mjs
@@ -2,13 +2,13 @@ import { Builder, By, until} from "selenium-webdriver";
 import { expect } from "chai";
 
 describe("Web Chat Application Tests", function () {
-  this.timeout(30000);
+  this.timeout(60000);
   let driver;
 
   before(async function () {
     driver = await new Builder().forBrowser("chrome").build();
     await driver.get("http://localhost:3000/email");
-    await driver.sleep(4000);
+    await driver.sleep(6000);
   });
 
   after(async function () {
@@ -18,18 +18,18 @@ describe("Web Chat Application Tests", function () {
   it("should login an existing user", async function () {
     const emailInput = await driver.findElement(By.id("email"));
     await emailInput.sendKeys("demo@gmail.com");
-    await driver.sleep(4000);
+    await driver.sleep(6000);
     const emailSubmitButton = await driver.findElement(
       By.className(
         "bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide"
       )
     );
     await emailSubmitButton.click();
-    await driver.sleep(3000);
+    await driver.sleep(6000);
 
     const passwordInput = await driver.findElement(By.id("password"));
     await passwordInput.sendKeys("1234");
-    await driver.sleep(3000);
+    await driver.sleep(6000);
 
     const passwordSubmitButton = await driver.findElement(
       By.className(
@@ -37,6 +37,6 @@ describe("Web Chat Application Tests", function () {
       )
     );
     await passwordSubmitButton.click();
-    await driver.sleep(2000);
+    await driver.sleep(6000);
   });
 });
